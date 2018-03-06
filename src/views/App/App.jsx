34b34723cbb71a322df80b058e8f3d458e59b8e1
MartynaStrzempek <martyna.strzempek@gmail.com>
2018-03-06 18:59:09 +0100

    add travis stuff

---
+++ src/views/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Container } from '../../utils/styledComponents';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+          Hello!
+      </Container>
+    );
+  }
+}
+
+export default App;
